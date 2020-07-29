1b186fc7f721dc83b5425dc1140bad2459857c04
Stefanny Paola Fracica Barboza <stefanny.fracica@ibm.com>
2020-07-29 17:43:28 -0300

    first commit

---
+++ src/AppClass.js
@@ -0,0 +1,65 @@
+import React,{ Component } from 'react';
+
+class App extends Component {
+  state = {
+    count: 0,
+    inOn: false,
+    x: null,
+    y: null
+  };
+  componentDidMount(){
+    document.title = `You have clicked ${this.state.count} times`
+    window.addEventListener('mousemove', this.handleMouseMove)
+  }
+  componentDidUpdate(){
+    document.title = `You have clicked ${this.state.count} times`
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('mousemove', this.handleMouseMove)
+  }
+
+  handleMouseMove = event => {
+    this.setState({
+      x: event.pageX,
+      y: event.pageY
+    })
+  }
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
+  }
+  incrementCount = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  render (){
+    return (
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>
+          I was clicked {this.state.count} times
+        </button>
+
+        <h2> Toggle Light</h2>
+        <div style={{
+          height: '50px',
+          width: '50px',
+          backgroundColor: this.state.isOn ? "yellow" : "grey"
+        }}
+        onClick={this.toggleLight}
+        >
+        </div>
+        <h2>Mouse Position</h2>
+        <p> X position: {this.state.x}</p>
+        <p> Y position: {this.state.y}</p>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Register.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+const initialFormState = {
+  username: "",
+  email: "",
+  password: ""
+}
+export default function Register() {
+  const [form, setForm] = useState(initialFormState);
+  const [user, setUser] = useState(null);
+
+  const handleChange = event => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setUser(form);
+    setForm(initialFormState);
+
+  }
+  return (
+    <div style={{
+      textAlign: 'center'
+    }}>
+      <h2>Register</h2>
+      <form
+      style={{
+        display: 'grid',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+      onSubmit={handleSubmit}
+      >
+        <input
+        type="text"
+        placeholder="Username"
+        name="username"
+        onChange={handleChange}
+        value={form.username}
+        />
+        <input
+        type="text"
+        placeholder="Email Address"
+        name="email"
+        onChange={handleChange}
+        value={form.email}
+        />
+        <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        onChange={handleChange}
+        value={form.password}
+        />
+        <button
+        type="submit"
+        >
+          Submit
+        </button>
+      </form>
+      {user && JSON.stringify(user, null, 2)}
+    </div>
+  )
+}
